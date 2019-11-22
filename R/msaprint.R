72d3b6da173cee31e59d774febf2f53a3a3393bb
lsyang <yanglinsensjtu@126.com>
2019-11-22 12:44:20 +0800

    add ranges

--- R/msaprint.R
+++ R/msaprint.R
@@ -1,15 +1,18 @@
 library(msa)
-msaprintPDF <- function(align.obj = align.obj, title = title){
+msaprintPDF <- function(align.obj = align.obj, title = title, ylim ){
   cd <- getwd()
   msatemp <- file.path(substr(cd,1,2),'msatemp')
   
   dir.create(msatemp)
   outdir  <-  file.path(msatemp,"alignements")
   fasta_file  <-  paste(outdir,".fasta",sep="")
+  a <- ylim
+  b <- a + 94
   
   output  <-  paste(outdir,"/",title,".tex", sep="")
   dir.create(outdir,showWarnings = FALSE)
   msaPrettyPrint(align.obj,
+                 y = c(a, b),
                  output="tex", 
                  showNames="left",
                  consensusColors = 'ColdHot',
