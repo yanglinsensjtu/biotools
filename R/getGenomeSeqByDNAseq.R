89393f4a98c20da7417513524b19cc366aba33f2
lsyang <yanglinsensjtu@126.com>
2019-12-20 17:18:23 +0800

    fix

--- R/getGenomeSeqByDNAseq.R
+++ R/getGenomeSeqByDNAseq.R
@@ -3,22 +3,28 @@ library(GenomicRanges)
 library(BSgenome.Hsapiens.UCSC.hg19)
 BS.hg19 <- BSgenome.Hsapiens.UCSC.hg19
 
-
-
 changeIRanges <- function(granges.obj = granges.obj, upstream = integer, width = integer){
-  GRanges(seqnames = granges.obj@seqnames,
-          IRanges(start = start(granges.obj@ranges) - upstream,
-                  width = width),
-          strand = granges.obj@strand,
-          seqinfo = granges.obj@seqinfo,
-          mcols(granges.obj))
+  if (as.character(granges.obj@strand) == '-') {
+    GRanges(seqnames = granges.obj@seqnames,
+            IRanges(end = end(granges.obj@ranges) + upstream - 1, 
+                    start = end(granges.obj@ranges) + upstream - width),
+            strand = granges.obj@strand,
+            seqinfo = granges.obj@seqinfo,
+            mcols(granges.obj))
+  }else{
+    GRanges(seqnames = granges.obj@seqnames,
+            IRanges(start = start(granges.obj@ranges) - upstream + 1, 
+                    width = width),
+            strand = granges.obj@strand,
+            seqinfo = granges.obj@seqinfo,
+            mcols(granges.obj))
+  }
 }
-
 getSeqFgenome <- function(seqstr = sequence,title = title, ...){
   seqstr <- seqstr
   seq <- DNAString(seqstr)
   chr.name <- seqnames(BS.hg19)
-  #chr.name <- sample(chr.name, length(chr.name), replace = F)
+  chr.name <- sample(chr.name, length(chr.name), replace = F)
   Flank.seq <- NA
   for (i in seq_len(length(chr.name))) {
     chr.seq <- getSeq(BS.hg19, chr.name[i])
@@ -35,14 +41,14 @@ getSeqFgenome <- function(seqstr = sequence,title = title, ...){
       Flank.seq <- getSeq(BS.hg19, location.c)
       names(Flank.seq) <- paste0(title,
                                  ' ',
-                                 location.c@seqnames,
+                                 location.c@seqnames, 
                                  ' ',
-                                 location.c@strand,
-                                 ':',
+                                 location.c@strand, 
+                                 ':', 
                                  ' ',
                                  location.c@ranges@start,
                                  '-',
-                                 location.c@ranges@start)
+                                 (location.c@ranges@width + location.c@ranges@start -1 ))
       print(paste0('The query sequence is sense strand according to the Genome ',chr.name[i]))
       break()
     }else if(length(mtpr)!=0){
@@ -51,40 +57,36 @@ getSeqFgenome <- function(seqstr = sequence,title = title, ...){
       location <- GRanges(seqnames = chr.name[i],
                           ranges = rangesr,
                           strand = strand)
-
+      
       location.c <- changeIRanges(location, ...)
       Flank.seq <- getSeq(BS.hg19, location.c)
       names(Flank.seq) <- paste0(title,
                                  ' ',
-                                 location.c@seqnames,
+                                 location.c@seqnames, 
                                  ' ',
-                                 location.c@strand,
-                                 ':',
+                                 location.c@strand, 
+                                 ':', 
                                  ' ',
                                  location.c@ranges@start,
                                  '-',
-                                 location.c@ranges@start)
+                                 (location.c@ranges@width + location.c@ranges@start -1 ))
       print(paste0('The query sequence is anti-sense strand according to the BS.hg19 Genome ',chr.name[i]))
       break()
     }else{
       print(paste0('Not found any location match the sequence in ',chr.name[i]))
     }
   }
-
+  
   if (!is.na(Flank.seq)) {
-
+    
     writeXStringSet(Flank.seq, filepath = paste0(title,'.fasta'))
     return(Flank.seq)
   }else{
     return(NA)
   }
-
+  
 }
 
-getSeqFgenome('', title = '',upstream = 100, width = 200)
-
-
-
-
+getSeqFgenome('ccttccagctcttctggctggggctgcactgcttgcgggctgccc', title = 'CD52 upstream',upstream = 500, width = 500) 
 
 
