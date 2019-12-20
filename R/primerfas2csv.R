51f6718e250a70245908a90a01d7977000e76585
lsyang <yanglinsensjtu@126.com>
2019-12-20 13:01:06 +0800

    change primer from primer3 to csv

--- R/primerfas2csv.R
+++ R/primerfas2csv.R
@@ -5,9 +5,10 @@ library(stringr)
 library(readr)
 primerfas2csv <- function(path = path){
   primer <- readDNAStringSet(path)
-  names(primer) <- str_extract(names(primer), '.*Primer..')
+  names(primer) <- str_extract(names(primer), '.*(?<=F|R)')
   primer <- as.data.frame(primer)
   path2 <- str_c(str_extract(as.character(path),'.*\\.'),'csv')
   write.csv(primer,file = path2)
 }
-primerfas2csv('../Primers_2019_10_10.fas')
+primerfas2csv('Primers_2019_12_18.fas')
+
