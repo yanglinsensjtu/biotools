f8a643e9f379838e68d574ecd88fb7fe084fdf19
lsyang <yanglinsensjtu@126.com>
2019-12-25 16:57:10 +0800

    fix

--- R/getGenomeSeqByDNAseq.R
+++ R/getGenomeSeqByDNAseq.R
@@ -20,24 +20,84 @@ changeIRanges <- function(granges.obj = granges.obj, upstream = integer, width =
             mcols(granges.obj))
   }
 }
-getSeqFgenome <- function(seqstr = sequence, title = NA,...){
+getSeqFgenome <- function(seqstr = sequence, 
+                          title = NA, 
+                          chrMatch = NA, 
+                          upstream = 0, 
+                          width = NA,
+                          ...){
+  
   seqstr <- seqstr
+  if (is.na(width)) {
+    width <- nchar(seqstr)
+  }
   seq <- DNAString(seqstr)
-  chr.name <- seqnames(BS.hg19)
-  chr.name <- sample(chr.name, length(chr.name), replace = F)
   Flank.seq <- NA
-  for (i in seq_len(length(chr.name))) {
-    chr.seq <- getSeq(BS.hg19, chr.name[i])
+  if (is.na(chrMatch)) {
+    chr.name <- seqnames(BS.hg19)
+    chr.name <- sample(chr.name, length(chr.name), replace = F)
+    for (i in seq_len(length(chr.name))) {
+      chr.seq <- getSeq(BS.hg19, chr.name[i])
+      seqr <- reverseComplement(seq)
+      mtp <- matchPattern(pattern = seq, chr.seq)
+      mtpr <- matchPattern(pattern = seqr, chr.seq)
+      if(length(mtp)!=0){
+        ranges <- mtp@ranges
+        strand <- '+'
+        location <- GRanges(seqnames = chr.name[i],
+                            ranges = ranges,
+                            strand = strand)
+        location.c <- changeIRanges(location, upstream, width)
+        Flank.seq <- getSeq(BS.hg19, location.c)
+        names(Flank.seq) <- paste0(title,
+                                   ' ',
+                                   location.c@seqnames,
+                                   ' ',
+                                   location.c@strand,
+                                   ':',
+                                   ' ',
+                                   location.c@ranges@start,
+                                   '-',
+                                   (location.c@ranges@width + location.c@ranges@start -1 ))
+        print(paste0('The query sequence is sense strand according to the Genome ',chr.name[i]))
+        #break()
+      }else if(length(mtpr)!=0){
+        rangesr <- mtpr@ranges
+        strand <- '-'
+        location <- GRanges(seqnames = chr.name[i],
+                            ranges = rangesr,
+                            strand = strand)
+        
+        location.c <- changeIRanges(location, upstream, width)
+        Flank.seq <- getSeq(BS.hg19, location.c)
+        names(Flank.seq) <- paste0(title,
+                                   ' ',
+                                   location.c@seqnames,
+                                   ' ',
+                                   location.c@strand,
+                                   ':',
+                                   ' ',
+                                   location.c@ranges@start,
+                                   '-',
+                                   (location.c@ranges@width + location.c@ranges@start ))
+        print(paste0('The query sequence is anti-sense strand according to the BS.hg19 Genome ',chr.name[i]))
+        #break()
+      }else{
+        print(paste0('Not found any location match the sequence in ',chr.name[i]))
+      }
+    }
+  }else{
+    chr.seq <- getSeq(BS.hg19, chrMatch)
     seqr <- reverseComplement(seq)
     mtp <- matchPattern(pattern = seq, chr.seq)
     mtpr <- matchPattern(pattern = seqr, chr.seq)
     if(length(mtp)!=0){
       ranges <- mtp@ranges
       strand <- '+'
-      location <- GRanges(seqnames = chr.name[i],
+      location <- GRanges(seqnames = chrMatch,
                           ranges = ranges,
                           strand = strand)
-      location.c <- changeIRanges(location, ...)
+      location.c <- changeIRanges(location, upstream, width)
       Flank.seq <- getSeq(BS.hg19, location.c)
       names(Flank.seq) <- paste0(title,
                                  ' ',
@@ -49,16 +109,15 @@ getSeqFgenome <- function(seqstr = sequence, title = NA,...){
                                  location.c@ranges@start,
                                  '-',
                                  (location.c@ranges@width + location.c@ranges@start -1 ))
-      print(paste0('The query sequence is sense strand according to the Genome ',chr.name[i]))
-      break()
+      print(paste0('The query sequence is sense strand according to the Genome ',chrMatch))
     }else if(length(mtpr)!=0){
       rangesr <- mtpr@ranges
       strand <- '-'
-      location <- GRanges(seqnames = chr.name[i],
+      location <- GRanges(seqnames = chrMatch,
                           ranges = rangesr,
                           strand = strand)
-
-      location.c <- changeIRanges(location, ...)
+      
+      location.c <- changeIRanges(location, upstream, width)
       Flank.seq <- getSeq(BS.hg19, location.c)
       names(Flank.seq) <- paste0(title,
                                  ' ',
@@ -70,12 +129,12 @@ getSeqFgenome <- function(seqstr = sequence, title = NA,...){
                                  location.c@ranges@start,
                                  '-',
                                  (location.c@ranges@width + location.c@ranges@start ))
-      print(paste0('The query sequence is anti-sense strand according to the BS.hg19 Genome ',chr.name[i]))
-      break()
+      print(paste0('The query sequence is anti-sense strand according to the BS.hg19 Genome ',chrMatch))
     }else{
-      print(paste0('Not found any location match the sequence in ',chr.name[i]))
+      print(paste0('Not found any location match the sequence in ',chrMatch))
     }
   }
+  
 
   if (!is.na(Flank.seq)) {
     if (!is.na(title)) {
@@ -87,11 +146,9 @@ getSeqFgenome <- function(seqstr = sequence, title = NA,...){
   }
 
 }
-s.seq <- 'TCCTGGTGCTCCCACAAAGGAGAAGGGCTGATCACTCAAAGTTGCGAACACCAAGCTCAACAATGAGCCCTGGAAAATTTCTGGAATGGATTATTAAACA'
-a <- getSeqFgenome(s.seq, title = 'chr1', upstream = 0, width = 20)
 
-s.seq.r <- 'GTCCCGGTTCCGCCTCCCCACTCCTGCGTCTTCCCGCCCCTGCCGGGTTCTGGGAAGCCTCGCGCGGCTCTTCCGCAGCTGCTGCCCGCCCGGAGCTCCT'
-b <- getSeqFgenome(s.seq.r, title = 'chr1.1',upstream = 0, width = 30)
-setequal(toString(a), s.seq)
-setequal(toString(b), s.seq.r)
 
+
+s.seq <- "GGAGGAAAAACTGTTTCATACAGAAGGCGT"
+
+getSeqFgenome(s.seq)
