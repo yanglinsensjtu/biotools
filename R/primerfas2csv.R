10ecf1bd9ea676bf1515c11fd0f379457ee3f3cc
lsyang <yanglinsensjtu@126.com>
2019-12-30 16:28:53 +0800

    improve

--- R/primerfas2csv.R
+++ R/primerfas2csv.R
@@ -3,12 +3,16 @@
 library(Biostrings)
 library(stringr)
 library(readr)
-primerfas2csv <- function(path = path){
+primerfas2csv <- function(path = path, prefix = NA){
+  prefix <- prefix
   primer <- readDNAStringSet(path)
   names(primer) <- str_extract(names(primer), '.*(?<=F|R)')
+  if(!is.na(prefix))
+  {
+    names(primer) <- str_c(prefix,names(primer))
+  }
   primer <- as.data.frame(primer)
   path2 <- str_c(str_extract(as.character(path),'.*\\.'),'csv')
   write.csv(primer,file = path2)
 }
-primerfas2csv('Primers_2019_12_18.fas')
-
+primerfas2csv('Primers_2019_12_30.fas', prefix = 'C2')
